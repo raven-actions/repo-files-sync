2f90c0d2a298d9eae994f45a1b3c49b13d42927a
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2026-05-18 13:48:46 -0700

    feat: enhance PR info logging
    Signed-off-by: Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -149,7 +149,7 @@ async function processRepo(git: Git, item: RepoConfig): Promise<string | undefin
       existingPr = OVERWRITE_EXISTING_PR ? await git.findExistingPr() : undefined;
 
       if (existingPr && !DRY_RUN) {
-        core.info(`Found existing PR ${existingPr.number}`);
+        core.info(`Found existing PR: ${existingPr.number} - ${existingPr.html_url}\n`);
         await git.setPrWarning();
       }
     }
